82f1683d7cad0e275407f8c87ebbe66586f0b19f
Oyinbayode <oyinbayode75@gmail.com>
2022-10-02 20:25:19 +0100

    feat: pull user data from firebase

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,4 +1,14 @@
 import { initializeApp } from "firebase/app";
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  serverTimestamp,
+  onSnapshot,
+  query,
+  orderBy,
+} from "firebase/firestore";
+import { GoogleAuthProvider, signInWithPopup, getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAImgIrmHmpkuPZc4CboYnYMR_FAjXiKms",
@@ -11,3 +21,66 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+async function loginWithGoogle() {
+  try {
+    const provider = new GoogleAuthProvider();
+    const auth = getAuth();
+
+    const { user } = await signInWithPopup(auth, provider);
+
+    return { uid: user.uid, displayName: user.displayName };
+  } catch (error) {
+    if (error.code !== "auth/cancelled-popup-request") {
+      console.error(error);
+    }
+
+    return null;
+  }
+}
+
+// Send a message to the Firebase DB
+async function SendMessage(roomId, user, text) {
+  try {
+    await addDoc(collection(db, "chat-rooms", roomId, "messages"), {
+      displayName: user,
+      text: text.trim(),
+      createdAt: serverTimestamp(),
+      id: Math.random().toString(36).substring(7),
+    });
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
+}
+
+async function AddUser(user) {
+  try {
+    await addDoc(collection(db, "users"), {
+      displayName: user,
+      createdAt: serverTimestamp(),
+    });
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
+}
+
+function getMessages(roomId, callback) {
+  console.log("getMessages", roomId);
+  return onSnapshot(
+    query(
+      collection(db, "chat-rooms", roomId, "messages"),
+      orderBy("timestamp", "asc")
+    ),
+    (querySnapshot) => {
+      const messages = querySnapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      console.log("messages", messages);
+      callback(messages);
+    }
+  );
+}
+
+export { SendMessage, db, getMessages, AddUser, loginWithGoogle };
